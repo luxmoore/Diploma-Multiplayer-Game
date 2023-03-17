7b399fbd99f3c306012ba14a6c1a79c7b86e1501
luxmoore <lukeluxmoore@gmail.com>
2023-03-17 22:12:03 +1100

    0.0.7.1
    > Reorganised files.
    - Deleted unneccessary and entirely unused scripts.
    
    / Preparing for tommorrow - I intend to get pathfinding working after just not doing that for a week or two.

--- Assets/LUX/Menus/Scripts/Game Setup Values.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class GameSetupValues
-{
-    // This script holds data, but cannot be used inside of a gameobject.
-}
