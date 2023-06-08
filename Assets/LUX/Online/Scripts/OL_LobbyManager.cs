b11d51341c11298aa8b1b44266c04cc59e75997d
luxmoore <lukeluxmoore@gmail.com>
2023-06-08 12:04:37 +1000

    b0.5

--- Assets/LUX/Online/Scripts/OL_LobbyManager.cs
+++ Assets/LUX/Online/Scripts/OL_LobbyManager.cs
@@ -4,15 +4,82 @@ using UnityEngine;
 
 public class OL_LobbyManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] gridBitViewBits;
+    public GameObject gridVoteIcon;
+    public bool[] readyStatus = new bool[3];
+
+    public bool gridVoteEnabledFromOther = false;
+    public bool gridVoteEnabledFromSelf = false;
+
+    #region BUTTONS
+    public void ButtonLeaveLobby()
     {
+        // netshit
+    }
+
+    public void ButtonReadyHost()
+    {
+        ReadyUp(0);
+    }
+
+    public void ButtonReadyClientA()
+    {
+        ReadyUp(1);
+    }
+
+    public void ButtonReadyClientB()
+    {
+        ReadyUp(2);
+    }
+
+    public void ButtonVoteGridChange()
+    {
+        if (gridVoteEnabledFromSelf)
+        {
+            // turn off and return.
+            return;
+        }
+
+        if (gridVoteEnabledFromOther)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+    #endregion
+
+    private void Update()
+    {
+        // refresh the grid if neccessary. On refresh, reset both gridVoteEnabled bools
         
     }
 
-    // Update is called once per frame
-    void Update()
+    #region FUNCTIONS
+
+    private void GridChangeUp()
     {
-        
+
     }
+
+    private void ReadyUp(int whichGuy)
+    {
+        if(readyStatus[whichGuy])
+        {
+            readyStatus[whichGuy] = false;
+        }
+        else
+        {
+            readyStatus[whichGuy] = true;
+        }
+
+        if(readyStatus[0] && readyStatus[1] && readyStatus[2])
+        {
+            // start game
+        }
+    }
+
+    #endregion
 }
